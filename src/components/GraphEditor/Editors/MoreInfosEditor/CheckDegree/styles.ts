0e2c789f8ed9f3c44b0600ab191efeb43dbc1eee
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-11-20 20:45:13 -0300

    Add button to get adjacent nodes (#11)

--- src/components/GraphEditor/Editors/MoreInfosEditor/CheckDegree/styles.ts
+++ src/components/GraphEditor/Editors/MoreInfosEditor/CheckDegree/styles.ts
@@ -12,16 +12,13 @@ export const ContainerDirected = styled.div`
   margin-top: 30px;
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 `;
 
 export const InfoContainer = styled.div`
     display: flex;
     flex-direction: row;
-    align-items: flex-start;
-    margin-right: -30px;
-    margin-left: -30px;
 `;
 
 export const Heading = styled.h2`
