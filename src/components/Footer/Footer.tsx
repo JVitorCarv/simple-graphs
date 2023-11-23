8132914c799cf46d0fb93c1aba634d045ca44b10
Matheus Barros <88664949+MatheusBarros23@users.noreply.github.com>
2023-11-23 19:37:15 -0300

    Add verify weight function

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,28 @@
-import React, { ChangeEvent, useCallback, useState } from 'react';
-import RenderIcon from '../GraphEditor/EditorContainer/components/RenderIcon/RenderIcon';
-import GithubLogo from '../../assets/github.png';
-import CytoscapeLogo from '../../assets/cytoscape.png';
-import { useCy } from '../../providers/useCy';
+import React, {  useCallback, useState } from "react";
+import RenderIcon from "../GraphEditor/EditorContainer/components/RenderIcon/RenderIcon";
+import GithubLogo from "../../assets/github.png";
+import CytoscapeLogo from "../../assets/cytoscape.png";
+import { useCy } from "../../providers/useCy";
 import {
   FooterContainer,
   IconsContainer,
   IconGroupContainer,
   LogoContainer,
   SelectedDirectionContainer,
-} from './styles';
-import ExportModal from './components/ModalComponents/ExportModalComponent';
-import ImportModal from './components/ModalComponents/ImportModalComponent';
-import SelectDirection from '../GraphEditor/EditorContainer/components/SelectDirection/SelectDirection';
-import { handleInputBatch, generateGraphFromUserInput, handleFileChange } from './functions/GraphFunctions'
-
+} from "./styles";
+import ExportModal from "./components/ModalComponents/ExportModalComponent";
+import ImportModal from "./components/ModalComponents/ImportModalComponent";
+import SelectDirection from "../GraphEditor/EditorContainer/components/SelectDirection/SelectDirection";
+import {
+  handleInputBatch,
+  generateGraphFromUserInput,
+  handleFileChange,
+} from "./functions/GraphFunctions";
 
 const Footer: React.FC = () => {
   const cy = useCy();
   const [modalIsOpen, setModalIsOpen] = useState(false);
-    
+
   const openModal = () => {
     setModalIsOpen(true);
   };
@@ -40,37 +43,38 @@ const Footer: React.FC = () => {
 
   const handleExportClick = useCallback(() => {
     const graphJson = cy.current.json();
-    const blob = new Blob([JSON.stringify(graphJson, null, 2)], {type: 'application/json'});
+    const blob = new Blob([JSON.stringify(graphJson, null, 2)], {
+      type: "application/json",
+    });
     const url = URL.createObjectURL(blob);
 
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = 'graph.json';
+    link.download = "graph.json";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-}, [cy]);
+  }, [cy]);
 
-const handleExportPngClick = useCallback(() => {
-    const pngContent = cy.current.png({output: 'blob', full: true, scale: 1});
+  const handleExportPngClick = useCallback(() => {
+    const pngContent = cy.current.png({ output: "blob", full: true, scale: 1 });
     const url = URL.createObjectURL(pngContent);
 
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = 'graph.png';
+    link.download = "graph.png";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-}, [cy]);
+  }, [cy]);
 
+  const inputFileRef = React.useRef<HTMLInputElement>(null);
 
-const inputFileRef = React.useRef<HTMLInputElement>(null);
-
-const handleFileClick = () => {
+  const handleFileClick = () => {
     inputFileRef.current?.click();
   };
-  
-const onGenerateClick = () => {
+
+  const onGenerateClick = () => {
     handleInputBatch(cy.current, generateGraphFromUserInput);
   };
 
@@ -78,10 +82,18 @@ const onGenerateClick = () => {
     <FooterContainer>
       <IconsContainer>
         <IconGroupContainer>
-          <a href="https://github.com/JVitorCarv/simple-graphs" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/JVitorCarv/simple-graphs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <LogoContainer src={GithubLogo} />
           </a>
-          <a href="https://js.cytoscape.org/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://js.cytoscape.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <LogoContainer src={CytoscapeLogo} />
           </a>
         </IconGroupContainer>
@@ -90,10 +102,29 @@ const onGenerateClick = () => {
         </SelectedDirectionContainer>
         <IconGroupContainer>
           <RenderIcon currentMode="Export" mode="Export" onClick={openModal} />
-          <ExportModal isOpen={modalIsOpen} closeModal={closeModal} handleExportClick={handleExportClick} handleExportPngClick={handleExportPngClick} />
-          <RenderIcon currentMode="Import" mode="Import" onClick={openModalImport} />
-          <input type="file" ref={inputFileRef} onChange={(event) => handleFileChange(cy.current, event)} style={{ display: 'none' }} />
-          <ImportModal isOpenImport={modalIsOpenImport} closeModalImport={closeModalImport} handleFileClick={handleFileClick} onGenerateClick={onGenerateClick}/>
+          <ExportModal
+            isOpen={modalIsOpen}
+            closeModal={closeModal}
+            handleExportClick={handleExportClick}
+            handleExportPngClick={handleExportPngClick}
+          />
+          <RenderIcon
+            currentMode="Import"
+            mode="Import"
+            onClick={openModalImport}
+          />
+          <input
+            type="file"
+            ref={inputFileRef}
+            onChange={(event) => handleFileChange(cy.current, event)}
+            style={{ display: "none" }}
+          />
+          <ImportModal
+            isOpenImport={modalIsOpenImport}
+            closeModalImport={closeModalImport}
+            handleFileClick={handleFileClick}
+            onGenerateClick={onGenerateClick}
+          />
         </IconGroupContainer>
       </IconsContainer>
     </FooterContainer>
--- src/components/Footer/functions/GraphFunctions.tsx
+++ src/components/Footer/functions/GraphFunctions.tsx
@@ -1,20 +1,23 @@
-import cytoscape from 'cytoscape';
+import cytoscape from "cytoscape";
 
-export const generateGraphFromUserInput = (cy: cytoscape.Core, input: string) => {
-  const lines = input.split('|');
+export const generateGraphFromUserInput = (
+  cy: cytoscape.Core,
+  input: string
+) => {
+  const lines = input.split("|");
   cy.elements().remove();
 
   const nodeIds = new Map<string, string>();
 
   lines.forEach((line, index) => {
-    const [source, target, weight] = line.split(' ');
+    const [source, target, weight] = line.split(" ");
 
     if (!nodeIds.has(source)) {
       const sourceNodeId = `node${nodeIds.size}`;
       nodeIds.set(source, sourceNodeId);
 
       cy.add({
-        group: 'nodes',
+        group: "nodes",
         data: { id: sourceNodeId, label: `${source}` },
         position: { x: 0, y: 0 },
       });
@@ -25,43 +28,60 @@ export const generateGraphFromUserInput = (cy: cytoscape.Core, input: string) =>
       nodeIds.set(target, targetNodeId);
 
       cy.add({
-        group: 'nodes',
+        group: "nodes",
         data: { id: targetNodeId, label: `${target}` },
         position: { x: 0, y: 0 },
       });
     }
 
+    const verifyWeight = (weight: any) => {
+      const parsedWeight = parseInt(weight);
+      if (Number.isNaN(parseInt(weight))) {
+        return 0;
+      } else {
+        return Math.abs(parsedWeight);
+      }
+    };
+
     const edgeId = `edge${index}`;
     cy.add({
-      group: 'edges',
+      group: "edges",
       data: {
         id: edgeId,
         source: nodeIds.get(source),
         target: nodeIds.get(target),
-        weight: parseInt(`${weight}`),
+        weight: verifyWeight(`${weight}`),
       },
     });
   });
-  return cy.layout({ name: 'grid' }).run();
+  return cy.layout({ name: "grid" }).run();
 };
-  
-  export const handleInputBatch = (cy: cytoscape.Core, generateGraphFromUserInput: (cy: cytoscape.Core, input: string) => void) => {
-    const userInput = prompt('Digite as informações para gerar o grafo (por exemplo, "0 2 3|0 1 2|1 2 3"):');
-    if (userInput) {
-      generateGraphFromUserInput(cy, userInput);
-    }
-  };
 
-  export const handleFileChange = (cy: cytoscape.Core, event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const contents = e.target?.result;
-            if (typeof contents === 'string') {
-                cy.json(JSON.parse(contents));
-            }
-        };
-        reader.readAsText(file);
-    }
+export const handleInputBatch = (
+  cy: cytoscape.Core,
+  generateGraphFromUserInput: (cy: cytoscape.Core, input: string) => void
+) => {
+  const userInput = prompt(
+    'Digite as informações para gerar o grafo (por exemplo, "0 2 3|0 1 2|1 2 3"):'
+  );
+  if (userInput) {
+    generateGraphFromUserInput(cy, userInput);
+  }
+};
+
+export const handleFileChange = (
+  cy: cytoscape.Core,
+  event: React.ChangeEvent<HTMLInputElement>
+) => {
+  const file = event.target.files?.[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const contents = e.target?.result;
+      if (typeof contents === "string") {
+        cy.json(JSON.parse(contents));
+      }
+    };
+    reader.readAsText(file);
+  }
 };
