55f5ae5b3de8665af959eebf1bd5af8976fa31df
lhckb <luis.carv.cruz@gmail.com>
2023-11-23 21:00:12 -0300

    implement secret requirement: get graph diameter

---
+++ src/components/GraphEditor/Editors/AlgorithmsEditor/components/CheckDiameter/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const InfoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  margin-right: 10px;
+`;
+
+export const InfoLabel = styled.h2`
+  margin-right: 10px;
+  color: black;
+  line-height: 1.5;
+`;
